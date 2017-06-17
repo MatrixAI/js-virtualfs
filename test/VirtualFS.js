ad5f6f65206af995534dd8a7c18d31928b1a7eda
Roger Qiu <roger.qiu@polyhack.io>
2017-06-18 00:58:50 +1000

    Added inodes and hardlinks, reworked the underlying data structures
    There is pending support for file descriptors and the standard
    open, close, read and write calls.
    
    Also pending is inode reuse through bit vector + ffs allocation.

--- test/VirtualFS.js
+++ test/VirtualFS.js
@@ -1,16 +1,17 @@
 var bl = require("bl");
 var should = require("should");
 
-var MemoryFile = require('../lib/MemoryFileSystem.js').MemoryFile;
-var MemorySymlink = require('../lib/MemoryFileSystem.js').MemorySymlink;
-var MemoryDir = require('../lib/MemoryFileSystem.js').MemoryDir;
-var MemoryFileSystem = require('../lib/MemoryFileSystem').MemoryFileSystem;
+var File = require('../lib/INodes').File;
+var Directory = require('../lib/INodes').Directory;
+var Symlink = require('../lib/INodes').Symlink;
+var VirtualFS = require('../lib/VirtualFS').FS;
 
 describe("directory", function() {
+
 	it("should have a empty root directory as startup", function(done) {
-		var fs = new MemoryFileSystem();
+		let fs = new VirtualFS();
 		fs.readdirSync("/").should.be.eql([]);
-		var stat = fs.statSync("/");
+		let stat = fs.statSync("/");
 		stat.isFile().should.be.eql(false);
 		stat.isDirectory().should.be.eql(true);
 		fs.readdir("/", function(err, files) {
@@ -19,44 +20,61 @@ describe("directory", function() {
 			done();
 		});
 	});
-	it("should make and remove directories", function() {
-		var fs = new MemoryFileSystem();
-		fs.mkdirSync("/test");
-		fs.mkdirSync("/test//sub/");
-		fs.mkdirpSync("/test/sub2");
-		fs.mkdirSync("/root\\dir");
+
+  it("it should make directories", function () {
+		let fs = new VirtualFS();
+		fs.mkdirSync("/first");
+		fs.mkdirSync("/first//sub/");
+		fs.mkdirpSync("/first/sub2");
+		fs.mkdirSync("/backslash\\dir");
 		fs.mkdirpSync("/");
-		(function () {
-			fs.mkdirSync("/");
-		}).should.throw();
-		fs.readdirSync("/").should.be.eql(["test", "root\\dir"]);
-		fs.readdirSync("/test/").should.be.eql(["sub", "sub2"]);
-		fs.rmdirSync("/test/sub//");
-		fs.readdirSync("//test").should.be.eql(["sub2"]);
-		fs.rmdirSync("/test/sub2");
-		fs.rmdirSync("/test");
-		fs.existsSync("/test").should.be.eql(false);
-		(function() {
-			fs.readdirSync("/test");
-		}).should.throw();
-		fs.readdirSync("/").should.be.eql(["root\\dir"]);
+		fs.readdirSync("/").should.be.eql(["first", "backslash\\dir"]);
+		fs.readdirSync("/first/").should.be.eql(["sub", "sub2"]);
 		fs.mkdirpSync("/a/depth/sub/dir");
 		fs.existsSync("/a/depth/sub").should.be.eql(true);
 		var stat = fs.statSync("/a/depth/sub");
 		stat.isFile().should.be.eql(false);
 		stat.isDirectory().should.be.eql(true);
+  });
+
+  it("it should not make the root directory", function () {
+		let fs = new VirtualFS();
+		(function () {
+			fs.mkdirSync("/");
+		}).should.throw();
+  });
+
+	it("should remove directories", function() {
+		let fs = new VirtualFS();
+		fs.mkdirSync("/first");
+		fs.mkdirSync("/first//sub/");
+		fs.mkdirpSync("/first/sub2");
+		fs.mkdirSync("/backslash\\dir");
+		fs.rmdirSync("/first/sub//");
+		fs.readdirSync("//first").should.be.eql(["sub2"]);
+		fs.rmdirSync("/first/sub2");
+		fs.rmdirSync('/first');
+		fs.existsSync("/first").should.be.eql(false);
+		(function() {
+			fs.readdirSync("/first");
+		}).should.throw();
+		fs.readdirSync("/").should.be.eql(["backslash\\dir"]);
 	});
+
 	it("should call a mkdir callback when passed as the third argument", function(done) {
-		var fs = new MemoryFileSystem();
+		var fs = new VirtualFS();
 		fs.mkdir('/test', {}, function(err) {
 			if (err) throw err;
 			done();
 		});
 	});
+
 });
+
 describe("files", function() {
+
 	it("should make and remove files", function() {
-		var fs = new MemoryFileSystem();
+		var fs = new VirtualFS();
 		fs.mkdirSync("/test");
 		var buf = new Buffer("Hello World", "utf-8");
 		fs.writeFileSync("/test/hello-world.txt", buf);
@@ -77,10 +95,36 @@ describe("files", function() {
 		fs.writeFileSync("/b", "Test", {encoding: "utf-8"});
 		fs.readFileSync("/b", "utf-8").should.be.eql("Test");
 	});
+
 });
+
+describe('hardlinks', function () {
+
+  it('should create multiple hardlinks to the same file', function () {
+		var fs = new VirtualFS();
+		fs.mkdirSync("/test");
+    fs.writeFileSync('/test/a');
+    fs.linkSync('/test/a', '/test/b');
+    let indexA = fs.statSync('/test/a').ino;
+    let indexB = fs.statSync('/test/b').ino;
+    indexA.should.be.eql(indexB);
+    fs.readFileSync('/test/a').should.be.eql(fs.readFileSync('/test/b'));
+  });
+
+  it('should not create hardlinks to directories', function () {
+		var fs = new VirtualFS();
+		fs.mkdirSync("/test");
+	  (function () {
+      fs.linkSync('/test', '/hardlinktotest');
+	  }).should.throw(/EPERM/);
+  });
+
+});
+
 describe("symlinks", function() {
+
 	it("should add and traverse symlinks", function() {
-		var fs = new MemoryFileSystem();
+		var fs = new VirtualFS();
 		fs.mkdirSync("/test");
 		var buf = new Buffer("Hello World", "utf-8");
 		fs.writeFileSync("/test/hello-world.txt", buf);
@@ -90,16 +134,18 @@ describe("symlinks", function() {
     fs.symlinkSync('/linktotestdir/hello-world.txt', '/linktofile');
     fs.readFileSync('/linktofile', 'utf-8').should.be.eql('Hello World');
 	});
+
 	it("should traverse relative symlinks", function() {
-		var fs = new MemoryFileSystem();
+		var fs = new VirtualFS();
 		fs.mkdirSync("/test");
 		var buf = new Buffer("Hello World", "utf-8");
     fs.writeFileSync('/a', buf);
     fs.symlinkSync('../a', '/test/linktoa');
     fs.readFileSync('/test/linktoa', 'utf-8').should.be.eql('Hello World');
 	});
+
   it ("it should delete only the symlink", function () {
-		var fs = new MemoryFileSystem();
+		var fs = new VirtualFS();
 		fs.mkdirSync("/test");
 		var buf = new Buffer("Hello World", "utf-8");
 		fs.writeFileSync("/test/hello-world.txt", buf);
@@ -109,10 +155,12 @@ describe("symlinks", function() {
     fs.unlinkSync('/linktofile');
     fs.readdirSync('/test').should.be.eql(['hello-world.txt']);
   });
+
 });
+
 describe("errors", function() {
 	it("should fail on invalid paths", function() {
-		var fs = new MemoryFileSystem();
+		var fs = new VirtualFS();
 		fs.mkdirpSync("/test/a/b/c");
 		fs.mkdirpSync("/test/a/bc");
 		fs.mkdirpSync("/test/abc");
@@ -156,14 +204,8 @@ describe("errors", function() {
 			err.should.be.instanceof(Error);
 		});
 	});
-	it("should fail incorrect arguments", function() {
-		var fs = new MemoryFileSystem();
-		(function() {
-			fs.writeFileSync("/test");
-		}).should.throw();
-	});
 	it("should fail on wrong type", function() {
-		var fs = new MemoryFileSystem();
+		var fs = new VirtualFS();
 		fs.mkdirpSync("/test/dir");
 		fs.mkdirpSync("/test/dir");
 		fs.writeFileSync("/test/file", "Hello");
@@ -197,7 +239,7 @@ describe("errors", function() {
 		fs.readdirSync("/test/").should.be.eql(["dir", "file"]);
 	});
 	it("should throw on readlink", function() {
-		var fs = new MemoryFileSystem();
+		var fs = new VirtualFS();
 		fs.mkdirpSync("/test/dir");
 		(function() {
 			fs.readlinkSync("/");
@@ -219,7 +261,7 @@ describe("errors", function() {
 describe("async", function() {
 	["stat", "readdir", "mkdirp", "rmdir", "unlink", "readlink"].forEach(function(methodName) {
 		it("should call " + methodName + " callback in a new event cycle", function(done) {
-			var fs = new MemoryFileSystem();
+			var fs = new VirtualFS();
 			var isCalled = false;
 			fs[methodName]('/test', function() {
 				isCalled = true;
@@ -230,7 +272,7 @@ describe("async", function() {
 	});
 	["mkdir", "readFile"].forEach(function(methodName) {
 		it("should call " + methodName + " a callback in a new event cycle", function(done) {
-			var fs = new MemoryFileSystem();
+			var fs = new VirtualFS();
 			var isCalled = false;
 			fs[methodName]('/test', {}, function() {
 				isCalled = true;
@@ -240,7 +282,7 @@ describe("async", function() {
 		});
 	});
 	it("should be able to use the async versions", function(done) {
-		var fs = new MemoryFileSystem();
+		var fs = new VirtualFS();
 		fs.mkdirp("/test/dir", function(err) {
 			if(err) throw err;
 			fs.writeFile("/test/dir/a", "Hello", function(err) {
@@ -264,7 +306,7 @@ describe("async", function() {
 		});
 	});
 	it("should return errors", function(done) {
-		var fs = new MemoryFileSystem();
+		var fs = new VirtualFS();
 		fs.readFile("/fail/file", function(err, content) {
 			err.should.be.instanceof(Error);
 			fs.writeFile("/fail/file", "", function(err) {
@@ -277,18 +319,18 @@ describe("async", function() {
 describe("streams", function() {
 	describe("writable streams", function() {
 		it("should write files", function() {
-			var fs = new MemoryFileSystem();
+			var fs = new VirtualFS();
 			fs.createWriteStream("/file").end("Hello");
 			fs.readFileSync("/file", "utf8").should.be.eql("Hello");
 		});
 		it("should zero files", function() {
-			var fs = new MemoryFileSystem();
+			var fs = new VirtualFS();
 			fs.createWriteStream("/file").end();
 			fs.readFileSync("/file", "utf8").should.be.eql("");
 		});
 		it("should accept pipes", function(done) {
 			// TODO: Any way to avoid the asyncness of this?
-			var fs = new MemoryFileSystem();
+			var fs = new VirtualFS();
 			bl(new Buffer("Hello"))
 				.pipe(fs.createWriteStream("/file"))
 				.once('finish', function() {
@@ -297,7 +339,7 @@ describe("streams", function() {
 				});
 		});
 		it("should propagate errors", function(done) {
-			var fs = new MemoryFileSystem();
+			var fs = new VirtualFS();
 			var stream = fs.createWriteStream("/file/unknown");
 			var err = false;
 			stream.once('error', function() {
@@ -311,7 +353,7 @@ describe("streams", function() {
 	});
 	describe("readable streams", function() {
 		it("should read files", function(done) {
-			var fs = new MemoryFileSystem();
+			var fs = new VirtualFS();
 			fs.writeFileSync("/file", "Hello");
 			fs.createReadStream("/file").pipe(bl(function(err, data) {
 				data.toString('utf8').should.be.eql("Hello");
@@ -319,7 +361,7 @@ describe("streams", function() {
 			}));
 		});
 		it("should respect start/end", function(done) {
-			var fs = new MemoryFileSystem();
+			var fs = new VirtualFS();
 			fs.writeFileSync("/file", "Hello");
 			fs.createReadStream("/file", {
 				start: 1,
@@ -330,7 +372,7 @@ describe("streams", function() {
 			}));
 		});
 		it("should propagate errors", function(done) {
-			var fs = new MemoryFileSystem();
+			var fs = new VirtualFS();
 			var stream = fs.createReadStream("file");
 			var err = false;
 			// Why does this dummy event need to be here? It looks like it
@@ -345,51 +387,4 @@ describe("streams", function() {
 			stream.read(0);
 		});
 	});
-});
-describe("os", function() {
-	var fileSystem;
-
-	beforeEach(function() {
-		fileSystem = new MemoryFileSystem({
-			a: new MemoryDir({
-				index: new MemoryFile(new Buffer("1")), // /a/index
-				dir: new MemoryDir({
-					index: new MemoryFile(new Buffer("2")) // a/dir/index
-				})
-			}),
-			"C:": new MemoryDir({
-				a: new MemoryDir({
-					index: new MemoryFile(new Buffer("3")), // C:\a\index
-					dir: new MemoryDir({
-						index: new MemoryFile(new Buffer("4")) // C:\a\dir\index
-					})
-				})
-			})
-		});
-	});
-
-	describe("unix", function() {
-		it("should stat stuff", function() {
-			fileSystem.statSync("/a").isDirectory().should.be.eql(true);
-			fileSystem.statSync("/a").isFile().should.be.eql(false);
-			fileSystem.statSync("/a/index").isDirectory().should.be.eql(false);
-			fileSystem.statSync("/a/index").isFile().should.be.eql(true);
-			fileSystem.statSync("/a/dir").isDirectory().should.be.eql(true);
-			fileSystem.statSync("/a/dir").isFile().should.be.eql(false);
-			fileSystem.statSync("/a/dir/index").isDirectory().should.be.eql(false);
-			fileSystem.statSync("/a/dir/index").isFile().should.be.eql(true);
-		});
-		it("should readdir directories", function() {
-			fileSystem.readdirSync("/a").should.be.eql(["index", "dir"]);
-			fileSystem.readdirSync("/a/dir").should.be.eql(["index"]);
-		});
-		it("should read files", function() {
-			fileSystem.readFileSync("/a/index", "utf-8").should.be.eql("1");
-			fileSystem.readFileSync("/a/dir/index", "utf-8").should.be.eql("2");
-		});
-		it("should also accept multi slashs", function() {
-			fileSystem.statSync("/a///dir//index").isFile().should.be.eql(true);
-		});
-	});
-
 });
