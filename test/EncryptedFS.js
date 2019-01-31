1f2d760e90840f0aa9208308bfc29eb6246dc881
Aashwin Varshney <aashwin.varshney@gmail.com>
2019-01-31 18:00:29 +1100

    WIP: primitive test cases for read() write() functions

--- test/EncryptedFS.js
+++ test/EncryptedFS.js
@@ -12,7 +12,7 @@ test('initialisation', t=> {
 test('open - sync', t => {
 	let efs = new EFS('very password');
 
-	let fd = efs.openSync('test/EncryptedFS.js');
+	let fd = efs.openSync('test/efs_test.txt');
 	console.log(typeof fd);
 	
 	// TODO: are there better tests than this?
@@ -23,10 +23,41 @@ test('open - sync', t => {
 test.cb('open - async', t => {
 	let efs = new EFS('very password');
 
-	let fd = efs.open('test/EncryptedFS.js', (err, fd) => {
+	let fd = efs.open('test/efs_test.txt', (err, fd) => {
 		t.true(err === null);
 		t.true((fd).constructor === Number);
 		t.end();
 	});
 	
+});
+
+test.serial('write - sync', t => {
+	let efs = new EFS('very password');
+
+	let fd = efs.openSync('test/test.txt');
+
+	const writeBuf = Buffer("Super confidential information");
+
+	console.log('write: ')
+	console.log(writeBuf);
+
+	efs.writeSync(fd, writeBuf)
+	t.pass();
+});
+
+test.serial('read - sync', t => {
+	let efs = new EFS('very password');
+
+	let fd = efs.openSync('test/test.txt');
+
+	const dummyBuffer = Buffer.alloc(0);
+
+	const pt = efs.readSync(fd, 'test/test.txt', dummyBuffer, 0, 1, 0);
+
+	console.log('read: ')
+	console.log(pt);
+
+	t.pass();
+
+
 });
