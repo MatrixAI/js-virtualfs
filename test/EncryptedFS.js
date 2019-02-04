9efdae06f412dadd926deac6c1467e032e6fdd88
Aashwin Varshney <aashwin.varshney@gmail.com>
2019-02-04 21:06:57 +1100

    WIP: many bug fixes ciphering correctly for single blocks

--- test/EncryptedFS.js
+++ test/EncryptedFS.js
@@ -31,33 +31,47 @@ test.cb('open - async', t => {
 	
 });
 
-test.serial('write - sync', t => {
+// TODO: find a way to unit test this method
+test('write - sync', t => {
+	t.pass();
 	let efs = new EFS('very password');
 
 	let fd = efs.openSync('test/test.txt');
 
 	const writeBuf = Buffer("Super confidential information");
 
-	console.log('write: ')
+	console.log('write: ');
 	console.log(writeBuf);
 
-	efs.writeSync(fd, writeBuf)
-	t.pass();
+	efs.writeSync(fd, writeBuf);
 });
 
-test.serial('read - sync', t => {
+// TODO: find a way to unit test this method
+test('read - sync', t => {
+	t.pass();
 	let efs = new EFS('very password');
 
 	let fd = efs.openSync('test/test.txt');
 
-	const dummyBuffer = Buffer.alloc(0);
+	const dummyBuffer = Buffer.alloc(10);
 
-	const pt = efs.readSync(fd, 'test/test.txt', dummyBuffer, 0, 1, 0);
+	const pt = efs.readSync(fd, dummyBuffer, 0, 1, 0);
 
-	console.log('read: ')
-	console.log(pt);
-
-	t.pass();
+});
 
 
+test('write then read', t => {
+	let efs = new EFS('very password');
+
+	let fd = efs.openSync('test/test.txt');
+
+	const writeBuffer = Buffer("Super confidential information");
+
+	efs.writeSync(fd, writeBuffer, 0, writeBuffer.length, 0);
+
+	let readBuffer = Buffer.alloc(4096);
+
+	efs.readSync(fd, readBuffer, 0, writeBuffer.length, 0);
+
+	t.deepEqual(writeBuffer, readBuffer.slice(0, writeBuffer.length));
 });
