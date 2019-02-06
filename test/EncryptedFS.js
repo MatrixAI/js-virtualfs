4dd92d6d777a785c22d908f513368ce2b06cab98
Aashwin Varshney <aashwin.varshney@gmail.com>
2019-02-06 19:00:09 +1100

    implemation of non block aligned writes and offset pamam. Also zero padding issue

--- test/EncryptedFS.js
+++ test/EncryptedFS.js
@@ -1,5 +1,6 @@
 import EFS from '../lib/EncryptedFS.js';
 import test from 'ava';
+import crypto from 'crypto';
 
 
 test('initialisation', t=> {
@@ -40,9 +41,6 @@ test('write - sync', t => {
 
 	const writeBuf = Buffer("Super confidential information");
 
-	console.log('write: ');
-	console.log(writeBuf);
-
 	efs.writeSync(fd, writeBuf);
 });
 
@@ -60,7 +58,7 @@ test('read - sync', t => {
 });
 
 
-test('write then read', t => {
+test('write then read - single block', t => {
 	let efs = new EFS('very password');
 
 	let fd = efs.openSync('test/test.txt');
@@ -69,9 +67,66 @@ test('write then read', t => {
 
 	efs.writeSync(fd, writeBuffer, 0, writeBuffer.length, 0);
 
-	let readBuffer = Buffer.alloc(4096);
+	let readBuffer = Buffer.alloc(writeBuffer.length);
 
 	efs.readSync(fd, readBuffer, 0, writeBuffer.length, 0);
 
-	t.deepEqual(writeBuffer, readBuffer.slice(0, writeBuffer.length));
+	t.deepEqual(writeBuffer, readBuffer);
 });
+
+test('write then read - multiple blocks', t => {
+	let efs = new EFS('very password');
+
+	let fd = efs.openSync('test/test.txt');
+
+	const blockSize = 4096;
+
+	const writeBuffer = crypto.randomBytes(blockSize * 3);
+
+	efs.writeSync(fd, writeBuffer, 0, writeBuffer.length, 0);
+
+	let readBuffer = Buffer.alloc(writeBuffer.length);
+
+	efs.readSync(fd, readBuffer, 0, writeBuffer.length, 0);
+
+	t.deepEqual(writeBuffer, readBuffer);
+});
+
+// TODO: this should really be split out into tests only concerning writes and tests only concerning reads
+test('write non-zero position - middle of start block', t => {
+	let efs = new EFS('very password');
+
+	const blockSize = 4096;
+
+
+	// write a three block file
+	const writePos = 2000;
+	const writeBuffer = crypto.randomBytes(blockSize * 3);
+	const fd = efs.openSync('test/test_middle.txt');
+	efs.writeSync(fd, writeBuffer, 0, writeBuffer.length, 0);
+
+	// write data in the middle
+	const middleData = Buffer('Malcom in the');	
+	efs.writeSync(fd, middleData, 0, middleData.length, writePos);
+
+	// re-read the blocks
+	let readBuffer = Buffer.alloc(blockSize * 3);
+	efs.readSync(fd, readBuffer, 0, readBuffer.length, 0);
+
+	middleData.copy(writeBuffer, writePos);
+	const expected = writeBuffer;
+
+	/*
+	console.log(expected.slice(0, blockSize).toString('hex'));
+	console.log('-------');
+	console.log(readBuffer.slice(0, blockSize).toString('hex'));
+	*/
+
+	t.true(readBuffer.equals(writeBuffer));
+	//t.deepEqual(expected.slice(0, blockSize), readBuffer.slice(0, blockSize));
+});
+
+/* TODO: middle of middle block(s)
+ * TODO: middle of last block
+ * TODO: single block such that .length < blocksize, .length + writeOffset > blocksize
+ */
